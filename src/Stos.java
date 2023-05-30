30b7cbe0a04cb4a2c50b2b55f1fd55e814fb98f0
SM4rtinho <sebastianmarut2@gmail.com>
2023-05-30 21:40:56 +0200

    Initial commit

---
+++ src/Stos.java
@@ -0,0 +1,40 @@
+import java.util.EmptyStackException;
+
+public class Stos {
+    private String[] elements;
+    private int top;
+
+    public Stos(int capacity) {
+        elements = new String[capacity];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == elements.length - 1;
+    }
+
+    public void push(String element) {
+        if (isFull()) {
+            throw new IllegalStateException("Stack is full");
+        }
+        elements[++top] = element;
+    }
+
+    public String pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return elements[top--];
+    }
+
+    public String top() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return elements[top];
+    }
+}
